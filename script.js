9e119c9bb547c7ec21fdeac42eb0181087e88dbc
Lại Phú Đức Long <long061020@gmail.com>
2022-02-18 22:36:08 +0700

    update for assignemtn

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let volume = document.getElementById("volume");
+    let rate = document.getElementById("rate");
+    let year = document.getElementById("year");
+    let result = document.getElementById("result");
+
+    //
+    console.log(document.getElementById("years").value);
+    rate.innerHTML =   document.getElementById("slide").value + "%";
+    year.innerHTML = Number(new Date().getFullYear()) +  Number(document.getElementById("years").value);
+    result.innerHTML = (Number(document.getElementById("slide").value)*Number(document.getElementById("years").value)*Number(document.getElementById("principal").value) + Number(document.getElementById("principal").value)).toFixed(2)
+    volume.innerHTML =  document.getElementById("principal").value;
 }
-        
+var slide = document.getElementById('slide');
+    
+
+// slide.onchange = function() {
+//     sliderDiv.innerHTML = this.value;
+//     return true;
+// }
+function showVal(value) {
+    let sliderDiv = document.getElementById("sliderAmount");
+    console.log(sliderDiv);
+    sliderDiv.innerHTML = value+"%";
+}
