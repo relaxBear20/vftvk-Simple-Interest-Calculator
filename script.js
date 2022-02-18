85a0c67c7f744981e261c2d70ff9d4eec0849b2a
Lại Phú Đức Long <long061020@gmail.com>
2022-02-18 23:00:27 +0700

    update for assignemtn

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-function compute()
-{
+function compute() {
     let volume = document.getElementById("volume");
     let rate = document.getElementById("rate");
     let year = document.getElementById("year");
     let result = document.getElementById("result");
-
+    const principle = document.getElementById("principal").value
+    if (principle) {
+        const amount = Number(principle);
+        if (amount <= 0) {
+            document.getElementById("principal").value = 0
+            document.getElementById("principal").focus()
+            alert("PLEASE ENTER POSITIVE AMOUNT")
+            return
+        }
+        
+    } else {
+        alert("PLEASE ENTER AMOUNT")
+        return
+    }
     //
-    console.log(document.getElementById("years").value);
-    rate.innerHTML =   document.getElementById("slide").value + "%";
-    year.innerHTML = Number(new Date().getFullYear()) +  Number(document.getElementById("years").value);
-    result.innerHTML = (Number(document.getElementById("slide").value)*Number(document.getElementById("years").value)*Number(document.getElementById("principal").value) + Number(document.getElementById("principal").value)).toFixed(2)
-    volume.innerHTML =  document.getElementById("principal").value;
+    rate.innerHTML = document.getElementById("slide").value + "%";
+    year.innerHTML = Number(new Date().getFullYear()) + Number(document.getElementById("years").value);
+    result.innerHTML = (Number(document.getElementById("slide").value) * Number(document.getElementById("years").value) * Number(document.getElementById("principal").value) + Number(document.getElementById("principal").value)).toFixed(2)
+    volume.innerHTML = document.getElementById("principal").value;
 }
 var slide = document.getElementById('slide');
-    
+
 
 // slide.onchange = function() {
 //     sliderDiv.innerHTML = this.value;
@@ -21,6 +32,6 @@ var slide = document.getElementById('slide');
 // }
 function showVal(value) {
     let sliderDiv = document.getElementById("sliderAmount");
-    console.log(sliderDiv);
-    sliderDiv.innerHTML = value+"%";
+    sliderDiv.innerHTML = value + "%";
 }
+
